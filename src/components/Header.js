c9f97d4cdd506ef89af1dd6310bcb97eb04df3f2
bilal1202 <cadet.bilal.87@gmail.com>
2023-04-26 22:45:55 +0500

    first

---
+++ src/components/Header.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBNavbarBrand,
+} from "mdb-react-ui-kit";
+import { useSelector, useDispatch } from "react-redux";
+import { setLogout } from "../redux/features/authSlice";
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(setLogout());
+  };
+  const { user } = useSelector((state) => ({ ...state.auth }));
+  return (
+    <MDBNavbar fixed="top" expand="lg" style={{ backgroundColor: "#222222" }}>
+      <MDBContainer>
+        <MDBNavbarBrand
+          href="/"
+          style={{ color: "#FFFFFF", fontWeight: "600", fontSize: "22px" }}
+        >
+
+<div style={{ width: '100px', height: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+  <img src="/images/White-logo_1.png" alt="Not Found" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+</div>
+      
+        </MDBNavbarBrand>
+        <MDBNavbarToggler
+          type="button"
+          aria-expanded="false"
+          aria-label="Toogle navigation"
+          onClick={() => setShow(!show)}
+          style={{ color: "#4CAF50" }}
+        >
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
+        <MDBCollapse show={show} navbar>
+          <MDBNavbarNav right fullWidth={false} className="mb-2 mb-lg-0">
+            {user?.result?._id && (
+              <h5 style={{ marginRight: "30px", marginTop: "17px" }}>
+                Logged in as: {user?.result?.name}
+              </h5>
+            )}
+            <MDBNavbarItem>
+              <MDBNavbarLink href="/">
+                <p className="header-text">Home</p>
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            {user?.result?._id && (
+              <>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/addTour">
+                    <p className="header-text">Add Tour</p>
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/dashboard">
+                    <p className="header-text">Dashboard</p>
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+              </>
+            )}
+            {user?.result?._id ? (
+              <MDBNavbarItem>
+                <MDBNavbarLink href="/" style={{ color: "#FFFFFF" }}>
+                  <p className="header-text" onClick={() => handleLogout()}>
+                    Logout
+                  </p>
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            ) : (
+              <MDBNavbarItem>
+                <MDBNavbarLink href="/login">
+                  <p className="header-text">Login</p>
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            )}
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+};
+
+export default Header;
